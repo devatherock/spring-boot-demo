b1d0f84cdd3ac067b18810df2ee0107e36a0db2e
devatherock <devatherock@gmail.com>
2022-05-14 22:25:05 -0500

    chore: Base spring boot demo application

---
+++ src/main/java/io/github/devatherock/Application.java
@@ -0,0 +1,14 @@
+package io.github.devatherock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.devatherock.Application;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
