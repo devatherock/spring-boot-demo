7f7945d0616f580c302b16bd93b500cfa44eb12f
devatherock <devatherock@gmail.com>
2022-08-13 07:20:23 -0500

    SO-73229897: Spring data mongo app

--- src/main/java/io/github/devatherock/Application.java
+++ src/main/java/io/github/devatherock/Application.java
@@ -2,6 +2,8 @@ package io.github.devatherock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.github.devatherock.Application;
 
@@ -10,5 +12,14 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/hello")
+        public String sayHello() {
+            return "Hello";
+        }
     }
 }
