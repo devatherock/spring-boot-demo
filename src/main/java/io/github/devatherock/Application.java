b1fa3adcf7e3ebca96ffc57293f69fcc1118e3b5
devatherock <devatherock@gmail.com>
2022-08-15 08:31:43 -0500

    SO-73348664: Initialize resource beans conditionally

--- src/main/java/io/github/devatherock/Application.java
+++ src/main/java/io/github/devatherock/Application.java
@@ -1,14 +1,40 @@
 package io.github.devatherock;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
-import io.github.devatherock.Application;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "env.name", havingValue = "dev")
+    public Resource resourceDev() throws IOException {
+        Resource resource = new ClassPathResource("logback-dev.xml");
+        LOGGER.info(Files.readString(resource.getFile().toPath()));
+        
+        return resource;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Resource resourceStage(@Value("classpath:logback.xml") Resource resource) throws IOException {
+        LOGGER.info(Files.readString(resource.getFile().toPath()));
+        return resource;
     }
 }
