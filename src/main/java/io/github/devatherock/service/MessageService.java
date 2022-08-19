4fd0e68fc415d4e93d82ab78ad15cdc4f5c01eb4
devatherock <devatherock@gmail.com>
2022-08-19 18:28:09 -0500

    SO-73088450: Clear cache in-between tests

--- src/main/java/io/github/devatherock/service/MessageService.java
+++ src/main/java/io/github/devatherock/service/MessageService.java
@@ -6,12 +6,16 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import io.github.devatherock.entities.Message;
+import lombok.Getter;
 
 @Component
 public class MessageService {
+    @Getter
+    private int callCounter = 0;
 
     @Cacheable(value = "testCache", key = "#id")
     public Message getMessage(String id) {
+        callCounter++;
         return new Message(id, new Date());
     }
 }
---
+++ src/test/groovy/io/github/devatherock/service/MessageServiceSpec.groovy
@@ -0,0 +1,43 @@
+package io.github.devatherock.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cache.CacheManager
+
+import spock.lang.Specification
+
+@SpringBootTest
+class MessageServiceSpec extends Specification {
+
+    @Autowired
+    MessageService service
+
+    @Autowired
+    CacheManager cacheManager
+
+    void setup() {
+        cacheManager.getCache('testCache').clear()
+    }
+
+    void 'test get message'() {
+        given:
+        String id = 'hello'
+
+        when:
+        service.getMessage(id)
+
+        then:
+        service.callCounter == 1
+    }
+
+    void 'test get message 2'() {
+        given:
+        String id = 'hello'
+
+        when:
+        service.getMessage(id)
+
+        then:
+        service.callCounter == 2
+    }
+}
