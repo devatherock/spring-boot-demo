cc5f39edc15e2d2f7c5abdd9128d965c99200b31
devatherock <devatherock@gmail.com>
2022-08-20 18:24:11 -0500

    SO-72682585: Ehcache without expiration

--- src/main/java/io/github/devatherock/service/TimeService.java
+++ src/main/java/io/github/devatherock/service/TimeService.java
@@ -2,6 +2,7 @@ package io.github.devatherock.service;
 
 import java.time.Instant;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class TimeService {
 
+    @Cacheable("testCache")
     public Instant getTime() {
         LOGGER.info("In getTime method");
         return Instant.now();
