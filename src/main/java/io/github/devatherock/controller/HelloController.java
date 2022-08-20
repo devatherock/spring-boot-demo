a2f82fe8666616186887dfb709e4402adcd6dbcc
devatherock <devatherock@gmail.com>
2022-08-20 12:50:34 -0500

    chore: Removed unused imports

--- src/main/java/io/github/devatherock/controller/HelloController.java
+++ src/main/java/io/github/devatherock/controller/HelloController.java
@@ -4,9 +4,6 @@ import java.io.StringWriter;
 
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
