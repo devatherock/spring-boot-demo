61a5d69c4ece1eaad1118a690b22ed44a6e60249
devatherock <devatherock@gmail.com>
2022-08-25 06:59:06 -0500

    SO-73448261: Programmatic CORS config

--- src/main/java/io/github/devatherock/Application.java
+++ src/main/java/io/github/devatherock/Application.java
@@ -2,13 +2,28 @@ package io.github.devatherock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.github.devatherock.Application;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @Profile("local")
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
