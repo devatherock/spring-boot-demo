bd890c670b239dba41079ebd64ebf5585c680cc4
devatherock <devatherock@gmail.com>
2023-05-02 15:10:50 -0500

    SO-75976503: NativeQuery with spring boot 3/spring data 5

--- src/main/java/io/github/devatherock/controller/HelloController.java
+++ src/main/java/io/github/devatherock/controller/HelloController.java
@@ -2,16 +2,15 @@ package io.github.devatherock.controller;
 
 import java.util.stream.Collectors;
 
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.sort.FieldSortBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.client.elc.QueryBuilders;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.query.FetchSourceFilterBuilder;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,10 +27,10 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		Pageable pageable = PageRequest.of(0, 2);
-		NativeSearchQuery query = new NativeSearchQueryBuilder()
+		NativeQuery query = NativeQuery.builder()
 			    .withSourceFilter(new FetchSourceFilterBuilder().withIncludes().build())
-				.withQuery(QueryBuilders.termQuery("total_quantity", "2"))
-				.withSorts(new FieldSortBuilder("order_date").order(SortOrder.ASC))
+				.withQuery(QueryBuilders.termQueryAsQuery("total_quantity", "2"))
+				.withSort(Sort.by(Direction.ASC, "order_date"))
 				.withPageable(pageable)
 				.build();
 		SearchHits<Accessory> results = elasticClient.search(query, Accessory.class);
