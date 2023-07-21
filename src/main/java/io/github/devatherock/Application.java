bdfe9d348ede5eef6e511e3c0b1854889a06dc56
devatherock <devatherock@gmail.com>
2023-07-21 08:50:41 -0500

    SO-76026874: Generated graal native image with maven

--- src/main/java/io/github/devatherock/Application.java
+++ src/main/java/io/github/devatherock/Application.java
@@ -1,14 +1,17 @@
 package io.github.devatherock;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.devatherock.Application;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        LOGGER.info("Starting my graal app");
         SpringApplication.run(Application.class, args);
     }
 }
