007079057e3c230992517fb797577d14889d460d
devatherock <devatherock@gmail.com>
2023-07-22 08:02:45 -0500

    SO-76740928: Respond with different content types in same endpoint

--- src/main/java/io/github/devatherock/controller/HelloController.java
+++ src/main/java/io/github/devatherock/controller/HelloController.java
@@ -1,19 +1,31 @@
 package io.github.devatherock.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import io.github.devatherock.service.TimeService;
 import lombok.RequiredArgsConstructor;
 
+import java.io.FileNotFoundException;
+
 @RestController
 @RequiredArgsConstructor
 public class HelloController {
-    private final TimeService timeService;
+    private final TimeService myService;
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello at " + timeService.getTime();
+    @GetMapping(path = "/test", produces = "application/pdf")
+    public ResponseEntity<StreamingResponseBody> getFile() throws FileNotFoundException {
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType("application/pdf"))
+                .body(myService.getFile());
     }
 
+    @GetMapping(path = "/test", produces = "application/json")
+    public Object getData() {
+        return myService.getData();
+    }
 }
--- src/main/java/io/github/devatherock/service/TimeService.java
+++ src/main/java/io/github/devatherock/service/TimeService.java
@@ -1,8 +1,14 @@
 package io.github.devatherock.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +16,19 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class TimeService {
 
-    public Instant getTime() {
-        LOGGER.info("In getTime method");
-        return Instant.now();
+    public StreamingResponseBody getFile() throws FileNotFoundException {
+        File file = new File("Path to the pdf file");
+        FileInputStream instream = new FileInputStream(file);
+
+        return out -> {
+            instream.transferTo(out);
+        };
+    }
+    
+    public Object getData() {
+        Map<String, String> someObject = new HashMap<>();
+        someObject.put("someKey", "someValue");
+        
+        return someObject;
     }
 }
