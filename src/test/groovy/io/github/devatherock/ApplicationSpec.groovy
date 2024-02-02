542d4c26a51ca891ad4fd9adbb4421f42bf7eac6
devatherock <devatherock@gmail.com>
2024-02-02 17:41:16 -0600

    fix: Updated spock and test to work with spring boot 3.x

--- src/test/groovy/io/github/devatherock/ApplicationSpec.groovy
+++ src/test/groovy/io/github/devatherock/ApplicationSpec.groovy
@@ -3,9 +3,11 @@ package io.github.devatherock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 @SpringBootTest
+@ContextConfiguration
 class ApplicationSpec extends Specification {
 
     @Autowired
