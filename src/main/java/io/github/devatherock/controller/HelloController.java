d0036850e2795e324f21cb8aa6c6f33158f7f6cd
devatherock <devatherock@gmail.com>
2024-02-17 10:06:41 -0600

    SO-24510145: Increased tomcat's form-urlencoded post body size

--- src/main/java/io/github/devatherock/controller/HelloController.java
+++ src/main/java/io/github/devatherock/controller/HelloController.java
@@ -1,11 +1,16 @@
 package io.github.devatherock.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.devatherock.service.TimeService;
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HelloController {
@@ -16,4 +21,11 @@ public class HelloController {
         return "Hello at " + timeService.getTime();
     }
 
+    @PostMapping("/images")
+    public void createImage(
+            @RequestParam String action,
+            @RequestParam String image
+    ) {
+        LOGGER.info("Action: {}, Image size: {}", action, image.length());
+    }
 }
